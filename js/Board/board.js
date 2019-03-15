f98f2f738c2a8cb438a8095e9547cc5d840dcf07
DrewE <andrew.engelstein@gmail.com>
2019-03-14 22:23:40 -0700

    Add additional hidden row to board to help with #gameOver function

--- js/Board/board.js
+++ js/Board/board.js
@@ -1,5 +1,5 @@
 class Board {
-  constructor(height = 20, width = 10) {
+  constructor(height = 21, width = 10) {
     this.height = height;
     this.width = width;
     this.grid = new Array(this.height)
