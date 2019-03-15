0f2955af5b3a0d622c674bc9f3477ac465f5a80b
DrewE <andrew.engelstein@gmail.com>
2019-03-14 22:42:49 -0700

    Update drawGrid to account for hidden top grid row

--- js/View/canvas-templates.js
+++ js/View/canvas-templates.js
@@ -6,12 +6,12 @@ export const drawGrid = grid => {
 
   c.clearRect(0, 0, 300, 600);
 
-  for (let i = 0; i < grid.length; i++) {
+  for (let i = 1; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
       c.fillStyle = grid[i][j] || "black";
-      c.fillRect(j * sq, i * sq, sq, sq);
+      c.fillRect(j * sq, (i - 1) * sq, sq, sq);
       c.strokeStyle = "rgba(255,255,255,0.2)";
-      c.strokeRect(j * sq, i * sq, sq, sq);
+      c.strokeRect(j * sq, (i - 1) * sq, sq, sq);
     }
   }
 };
