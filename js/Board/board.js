1b05560924fff3726cbbd5aa01eb1a39421d3fd1
DrewE <andrew.engelstein@gmail.com>
2019-03-15 11:43:04 -0700

    Add #setCurrentPiece and #placeCurrentPiece to Board

--- js/Board/board.js
+++ js/Board/board.js
@@ -5,7 +5,44 @@ class Board {
     this.grid = new Array(this.height)
       .fill()
       .map(r => new Array(this.width).fill(0));
+    this.currentPiece = undefined;
+    this.currentPos = [];
   }
+
+  setCurrentPiece(piece) {
+    this.currentPiece = piece;
+  }
+
+  placeCurrentPiece() {
+    this.currentPiece.orientations[0].forEach((block, idx) => {
+      let row = 0 + block[0];
+      let col = 5 + block[1];
+      this.grid[row][col] = this.currentPiece.color;
+      this.currentPos.push([row, col]);
+    });
+  }
+
+  moveLeft() {
+    console.log("left");
+  }
+
+  moveRight() {
+    console.log("right");
+  }
+
+  moveDown() {
+    console.log("down");
+  }
+
+  rotateClockwise() {
+    console.log("clockwise");
+  }
+
+  rotateCounterClockwise() {
+    console.log("counter-clockwise");
+  }
+
+  isValidMove(currPos, endPos) {}
 }
 
 export default Board;
