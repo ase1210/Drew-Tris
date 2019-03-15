01b7a8abac63a4c2d78c38f22c5b564ba0f50b5a
DrewE <andrew.engelstein@gmail.com>
2019-03-15 11:00:23 -0700

    Add function structure for Pieces parent class

--- js/Pieces/pieces.js
+++ js/Pieces/pieces.js
@@ -1,3 +1,32 @@
 class Piece {
-  draw(x, y, color) {}
+  moveLeft() {
+    console.log("left");
+  }
+
+  moveRight() {
+    console.log("right");
+  }
+
+  moveDown() {
+    console.log("down");
+  }
+
+  rotateClockwise() {
+    console.log("clockwise");
+  }
+
+  rotateCounterClockwise() {
+    console.log("counter-clockwise");
+  }
+}
+
+class IPiece extends Piece {
+  constructor(color) {
+    super(props);
+    this.color = color;
+    this.orientations = [
+      [[0, -1], [0, 0], [0, 1], [0, 2]],
+      [[0, 0], [-1, 0], [-2, 0], [-3, 0]]
+    ];
+  }
 }
