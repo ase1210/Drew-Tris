abf65be1d75b0c55b185d59870a80f009732bf6c
DrewE <andrew.engelstein@gmail.com>
2019-03-14 19:52:43 -0700

    Update entry file to simulate animation of grid

--- tetris.js
+++ tetris.js
@@ -20,13 +20,26 @@ let grid = [
   ["#A0E8B5", "#A0E8B5", 0, 0, 0, 0, 0, 0, 0, 0],
   ["#A0E8B5", 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ["#A0E8B5", "#FFF19C", 0, 0, 0, 0, 0, 0, 0, 0],
-  ["#FFF19C", "#FFF19C", "#FFF19C", 0, 0, 0, 0, 0, 0, 0]
+  ["#FFF19C", "#FFF19C", "#FFF19C", 0, 0, 0, "#F0B23C", "#AFC1B9", 0, 0]
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.getElementById("canvas");
+  // const canvas = document.getElementById("canvas");
 
-  const c = canvas.getContext("2d");
+  // const c = canvas.getContext("2d");
+  // window.drawGrid = drawGrid;
+  // window.grid = grid;
+
+  function animate() {
+    console.log("animate");
+    let last = grid.pop();
+    grid.unshift(last);
+    // grid[19][9] = "#FFF19C";
+    drawGrid(grid);
+  }
+  // window.animate = animate;
 
   drawGrid(grid);
+
+  setInterval(animate, 1000);
 });
