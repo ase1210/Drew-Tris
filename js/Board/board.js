f16b364ac7351272b2c80a25912a03607cfc1a93
DrewE <andrew.engelstein@gmail.com>
2019-03-14 22:22:02 -0700

    Add board class with constructor functiongs

---
+++ js/Board/board.js
@@ -0,0 +1,9 @@
+class Board {
+  constructor(height = 20, width = 10) {
+    this.height = height;
+    this.width = width;
+    this.grid = new Array(this.height)
+      .fill()
+      .map(r => new Array(this.width).fill(0));
+  }
+}
