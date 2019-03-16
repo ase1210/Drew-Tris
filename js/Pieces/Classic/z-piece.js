ea0234a44dca56187cd46594f0eddf60c612b807
DrewE <andrew.engelstein@gmail.com>
2019-03-16 12:44:08 -0700

    Add ZPiece class

---
+++ js/Pieces/Classic/z-piece.js
@@ -0,0 +1,13 @@
+import Piece from "../pieces";
+
+export default class ZPiece extends Piece {
+  constructor(color) {
+    super(color);
+    this.initialState = [[1, 1], [1, 0], [0, 0], [0, -1]];
+    this.numStates = 2;
+    this.rotationMap = {
+      0: [[0, 2], [1, 1], [0, 0], [1, -1]],
+      1: [[0, -2], [-1, -1], [0, 0], [-1, 1]]
+    };
+  }
+}
