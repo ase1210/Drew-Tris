af4d500e9524c162da195419f49999f5d5f293de
DrewE <andrew.engelstein@gmail.com>
2019-03-15 23:01:48 -0700

    Add Piece methods #directionsMap(key) and #move(direction, currPos)

--- js/Pieces/pieces.js
+++ js/Pieces/pieces.js
@@ -2,4 +2,24 @@ export default class Piece {
   constructor(color) {
     this.color = color;
   }
+
+  directionsMap(key) {
+    // k:v  === direction: [row/col index, shift]
+    const directions = {
+      right: { idx: 1, shift: 1 },
+      left: { idx: 1, shift: -1 },
+      down: { idx: 0, shift: 1 }
+    };
+    return directions[key];
+  }
+
+  move(direction, currPos) {
+    const newPos = currPos.map(block => {
+      let newBlock = block.slice();
+      const { idx, shift } = this.directionsMap(direction);
+      newBlock[idx] += shift;
+      return newBlock;
+    });
+    return newPos;
+  }
 }
