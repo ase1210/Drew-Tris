566d0c94cf1258b67a9b5ddf28eb6c64d7577f62
DrewE <andrew.engelstein@gmail.com>
2019-03-16 13:11:11 -0700

    Add LPiece class

---
+++ js/Pieces/Classic/l-piece.js
@@ -0,0 +1,15 @@
+import Piece from "../pieces";
+
+export default class LPiece extends Piece {
+  constructor(color) {
+    super(color);
+    this.initialState = [[0, -1], [0, 0], [0, 1], [1, -1]];
+    this.numStates = 4;
+    this.rotationMap = {
+      0: [[-1, -1], [0, 0], [1, 1], [0, -2]],
+      1: [[-1, 1], [0, 0], [1, -1], [-2, 0]],
+      2: [[2, 1], [1, 0], [0, -1], [1, 2]],
+      3: [[0, -1], [-1, 0], [-2, 1], [1, 0]]
+    };
+  }
+}
