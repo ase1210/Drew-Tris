dec802f09a81917d613dadb65773d149c6ab9b99
DrewE <andrew.engelstein@gmail.com>
2019-03-16 12:48:32 -0700

    Add OPiece class

---
+++ js/Pieces/Classic/o-piece.js
@@ -0,0 +1,12 @@
+import Piece from "../pieces";
+
+export default class OPiece extends Piece {
+  constructor(color) {
+    super(color);
+    this.initialState = [[1, -1], [1, 0], [0, 0], [0, -1]];
+    this.numStates = 1;
+    this.rotationMap = {
+      0: [[0, 0], [0, 0], [0, 0], [0, 0]]
+    };
+  }
+}
