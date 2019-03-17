416761ee5f1e7d49972a9591a8b1c06b283d2e5b
DrewE <andrew.engelstein@gmail.com>
2019-03-17 12:10:18 -0700

    Add pentis XPiece

---
+++ js/Pieces/Pentis/x-piece.js
@@ -0,0 +1,13 @@
+import Piece from "../pieces";
+
+export default class XPiece extends Piece {
+  constructor(color) {
+    super(color);
+    this.initialState = [[-2, -1], [-2, 1], [-1, 0], [0, -1], [0, 1]];
+    this.numStates = 2;
+    this.rotationMap = {
+      0: [[-1, 0], [0, 1], [0, 0], [0, -1], [1, 0]],
+      1: [[1, 0], [0, -1], [0, 0], [0, 1], [-1, 0]]
+    };
+  }
+}
