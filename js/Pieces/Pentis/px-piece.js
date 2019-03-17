30938176920e7a6babe96887ab401b93e1a0b733
DrewE <andrew.engelstein@gmail.com>
2019-03-17 12:17:48 -0700

    Add PZPiece and rename PXPiece

--- js/Pieces/Pentis/px-piece.js
+++ js/Pieces/Pentis/px-piece.js
@@ -1,6 +1,6 @@
 import Piece from "../pieces";
 
-export default class XPiece extends Piece {
+export default class PXPiece extends Piece {
   constructor(color) {
     super(color);
     this.initialState = [[-2, -1], [-2, 1], [-1, 0], [0, -1], [0, 1]];
