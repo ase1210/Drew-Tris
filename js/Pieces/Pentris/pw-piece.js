cdf82874c715393f50d3db6d2249f70f04c97b8e
DrewE <andrew.engelstein@gmail.com>
2019-03-17 14:13:43 -0700

    Add PTPiece and PWPiece

---
+++ js/Pieces/Pentris/pw-piece.js
@@ -0,0 +1,15 @@
+import Piece from "../pieces";
+
+export default class PWPiece extends Piece {
+  constructor(color) {
+    super(color);
+    this.initialState = [[-2, -1], [-1, -1], [-1, 0], [0, 0], [0, 1]];
+    this.numStates = 4;
+    this.rotationMap = {
+      0: [[-2, 0], [-1, -1], [0, 0], [1, -1], [2, 0]],
+      1: [[0, 2], [-1, 1], [0, 0], [-1, -1], [0, -2]],
+      2: [[2, 0], [1, 1], [0, 0], [-1, 1], [-2, 0]],
+      3: [[0, -2], [1, -1], [0, 0], [1, 1], [0, 2]]
+    };
+  }
+}
