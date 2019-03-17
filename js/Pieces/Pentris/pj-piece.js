f3fad60b63714e4fbccb8060203927f6b84122df
DrewE <andrew.engelstein@gmail.com>
2019-03-17 15:15:32 -0700

    Add PJPiece and PLPiece

---
+++ js/Pieces/Pentris/pj-piece.js
@@ -0,0 +1,15 @@
+import Piece from "../pieces";
+
+export default class PJPiece extends Piece {
+  constructor(color) {
+    super(color);
+    this.initialState = [[0, -2], [0, -1], [0, 0], [0, 1], [-1, 0]];
+    this.numStates = 4;
+    this.rotationMap = {
+      0: [[0, -2], [1, -1], [2, 0], [3, 1], [1, 1]],
+      1: [[-3, 1], [-2, 0], [-1, -1], [0, -2], [0, 0]],
+      2: [[2, 2], [1, 1], [0, 0], [-1, -1], [1, -1]],
+      3: [[1, -1], [0, 0], [-1, 1], [-2, 2], [-2, 0]]
+    };
+  }
+}
---
+++ js/Pieces/Pentris/pl-piece.js
@@ -0,0 +1,15 @@
+import Piece from "../pieces";
+
+export default class PLPiece extends Piece {
+  constructor(color) {
+    super(color);
+    this.initialState = [[0, -2], [0, -1], [0, 0], [0, 1], [-1, -1]];
+    this.numStates = 4;
+    this.rotationMap = {
+      0: [[0, -2], [1, -1], [2, 0], [3, 1], [0, 0]],
+      1: [[-3, 1], [-2, 0], [-1, -1], [0, -2], [-1, 1]],
+      2: [[2, 2], [1, 1], [0, 0], [-1, -1], [2, 0]],
+      3: [[1, -1], [0, 0], [-1, 1], [-2, 2], [-1, -1]]
+    };
+  }
+}
