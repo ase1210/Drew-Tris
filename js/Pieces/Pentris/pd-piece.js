0041c527336bd0c057e91e835f33f2565bd93fa4
DrewE <andrew.engelstein@gmail.com>
2019-03-17 13:31:18 -0700

    Add PDPiece and fix PBPiece rotation map

---
+++ js/Pieces/Pentris/pd-piece.js
@@ -0,0 +1,15 @@
+import Piece from "../pieces";
+
+export default class PDPiece extends Piece {
+  constructor(color) {
+    super(color);
+    this.initialState = [[-1, -1], [0, -1], [-1, 0], [0, 0], [-2, -1]];
+    this.numStates = 4;
+    this.rotationMap = {
+      0: [[-1, -1], [0, -2], [0, 0], [1, -1], [-2, 0]],
+      1: [[0, 1], [-1, 0], [1, 0], [0, -1], [1, 2]],
+      2: [[0, 0], [-1, 1], [-1, -1], [-2, 0], [1, -1]],
+      3: [[1, 0], [2, 1], [0, 1], [1, 2], [0, -1]]
+    };
+  }
+}
