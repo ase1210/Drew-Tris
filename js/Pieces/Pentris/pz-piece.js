ec8320ef9970595e93979752a24c7cd7acb50700
DrewE <andrew.engelstein@gmail.com>
2019-03-23 21:18:07 -0700

    Update PZPiece orientation and rotation map

--- js/Pieces/Pentris/pz-piece.js
+++ js/Pieces/Pentris/pz-piece.js
@@ -3,15 +3,17 @@ import Piece from "../pieces";
 export default class PZPiece extends Piece {
   constructor(color) {
     super(color);
-    this.height = 3;
-    this.width = 3;
-    this.x = -1;
-    this.y = -2;
-    this.initialState = [[-2, -1], [-2, 0], [-1, 0], [0, 0], [0, 1]];
-    this.numStates = 2;
+    this.height = 2;
+    this.width = 4;
+    this.x = -2;
+    this.y = -1;
+    this.initialState = [[-1, -2], [-1, -1], [-1, 0], [0, 0], [0, 1]];
+    this.numStates = 4;
     this.rotationMap = {
-      0: [[-2, 0], [-1, 1], [0, 0], [1, -1], [2, 0]],
-      1: [[2, 0], [1, -1], [0, 0], [-1, 1], [-2, 0]]
+      0: [[-1, -1], [0, 0], [1, 1], [2, 0], [3, 1]],
+      1: [[-2, 2], [-1, 1], [0, 0], [-1, -1], [0, -2]],
+      2: [[3, 1], [2, 0], [1, -1], [0, 0], [-1, -1]],
+      3: [[0, -2], [-1, -1], [-2, 0], [-1, 1], [-2, 2]]
     };
   }
 }
