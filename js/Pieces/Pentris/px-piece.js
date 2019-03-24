32e662b880be3e4cac8c87692786fac0e73a1915
DrewE <andrew.engelstein@gmail.com>
2019-03-23 22:00:02 -0700

    Update PXPiece to be static

--- js/Pieces/Pentris/px-piece.js
+++ js/Pieces/Pentris/px-piece.js
@@ -7,11 +7,10 @@ export default class PXPiece extends Piece {
     this.width = 3;
     this.x = -1;
     this.y = -2;
-    this.initialState = [[-2, -1], [-2, 1], [-1, 0], [0, -1], [0, 1]];
-    this.numStates = 2;
+    this.initialState = [[-2, 0], [-1, -1], [-1, 0], [-1, 1], [0, 0]];
+    this.numStates = 1;
     this.rotationMap = {
-      0: [[-1, 0], [0, 1], [0, 0], [0, -1], [1, 0]],
-      1: [[1, 0], [0, -1], [0, 0], [0, 1], [-1, 0]]
+      0: [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
     };
   }
 }
