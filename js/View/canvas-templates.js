50c921e6342e777b911496952a1891c8e3333404
DrewE <andrew.engelstein@gmail.com>
2019-03-23 18:38:03 -0700

    Add #drawGamePreview with static pieces mapped

--- js/View/canvas-templates.js
+++ js/View/canvas-templates.js
@@ -17,6 +17,106 @@ export const drawGrid = grid => {
   }
 };
 
+export const drawGamePreview = (colors, pieces) => {
+  const canvas = document.getElementById("tetris");
+  const c = canvas.getContext("2d");
+  const sq = canvas.width / 10;
+  canvas.width = sq * 10;
+  canvas.height = sq * 20;
+
+  c.fillStyle = "red";
+  c.fillRect(5 * sq, 20, sq, sq);
+  c.fillRect(6 * sq, 20, sq, sq);
+  c.fillRect(4 * sq, 20, sq, sq);
+  c.fillRect(5 * sq, sq + 20, sq, sq);
+  c.lineWidth = 2;
+  c.strokeStyle = "black";
+  c.strokeRect(5 * sq, 20, sq, sq);
+  c.strokeRect(6 * sq, 20, sq, sq);
+  c.strokeRect(4 * sq, 20, sq, sq);
+  c.strokeRect(5 * sq, sq + 20, sq, sq);
+
+  // Long Piece with Canvas
+  c.fillStyle = "blue";
+  c.fillRect(5 * sq, 2 * sq + 30, sq, sq);
+  c.fillRect(6 * sq, 2 * sq + 30, sq, sq);
+  c.fillRect(4 * sq, 2 * sq + 30, sq, sq);
+  c.fillRect(7 * sq, 2 * sq + 30, sq, sq);
+  c.lineWidth = 2;
+  c.strokeStyle = "black";
+  c.strokeRect(5 * sq, 2 * sq + 30, sq, sq);
+  c.strokeRect(6 * sq, 2 * sq + 30, sq, sq);
+  c.strokeRect(4 * sq, 2 * sq + 30, sq, sq);
+  c.strokeRect(7 * sq, 2 * sq + 30, sq, sq);
+
+  // Z Piece with Canvas
+  c.fillStyle = "green";
+  c.fillRect(5 * sq, 3 * sq + 40, sq, sq);
+  c.fillRect(6 * sq, 4 * sq + 40, sq, sq);
+  c.fillRect(5 * sq, 4 * sq + 40, sq, sq);
+  c.fillRect(4 * sq, 3 * sq + 40, sq, sq);
+  c.lineWidth = 2;
+  c.strokeStyle = "black";
+  c.strokeRect(5 * sq, 3 * sq + 40, sq, sq);
+  c.strokeRect(6 * sq, 4 * sq + 40, sq, sq);
+  c.strokeRect(5 * sq, 4 * sq + 40, sq, sq);
+  c.strokeRect(4 * sq, 3 * sq + 40, sq, sq);
+
+  // Reverse Z Piece with Canvas
+  c.fillStyle = "indigo";
+  c.fillRect(5 * sq, 5 * sq + 130, sq, sq);
+  c.fillRect(6 * sq, 5 * sq + 130, sq, sq);
+  c.fillRect(5 * sq, 6 * sq + 130, sq, sq);
+  c.fillRect(4 * sq, 6 * sq + 130, sq, sq);
+  c.lineWidth = 2;
+  c.strokeStyle = "black";
+  c.strokeRect(5 * sq, 5 * sq + 130, sq, sq);
+  c.strokeRect(6 * sq, 5 * sq + 130, sq, sq);
+  c.strokeRect(5 * sq, 6 * sq + 130, sq, sq);
+  c.strokeRect(4 * sq, 6 * sq + 130, sq, sq);
+
+  // L Piece with Canvas
+  c.fillStyle = "yellow";
+  c.fillRect(5 * sq, 7 * sq + 140, sq, sq);
+  c.fillRect(6 * sq, 7 * sq + 140, sq, sq);
+  c.fillRect(4 * sq, 7 * sq + 140, sq, sq);
+  c.fillRect(4 * sq, 8 * sq + 140, sq, sq);
+  c.lineWidth = 2;
+  c.strokeStyle = "black";
+  c.strokeRect(5 * sq, 7 * sq + 140, sq, sq);
+  c.strokeRect(6 * sq, 7 * sq + 140, sq, sq);
+  c.strokeRect(4 * sq, 7 * sq + 140, sq, sq);
+  c.strokeRect(4 * sq, 8 * sq + 140, sq, sq);
+
+  // Reverse L Piece with Canvas
+  c.fillStyle = "orange";
+  c.fillRect(5 * sq, 9 * sq + 150, sq, sq);
+  c.fillRect(6 * sq, 9 * sq + 150, sq, sq);
+  c.fillRect(4 * sq, 9 * sq + 150, sq, sq);
+  c.fillRect(6 * sq, 10 * sq + 150, sq, sq);
+  c.lineWidth = 2;
+  c.strokeStyle = "black";
+  c.strokeRect(5 * sq, 9 * sq + 150, sq, sq);
+  c.strokeRect(6 * sq, 9 * sq + 150, sq, sq);
+  c.strokeRect(4 * sq, 9 * sq + 150, sq, sq);
+  c.strokeRect(6 * sq, 10 * sq + 150, sq, sq);
+
+  // Square Piece with Canvas
+  c.fillStyle = "violet";
+  c.fillRect(5 * sq, 11 * sq + 160, sq, sq);
+  c.fillRect(6 * sq, 11 * sq + 160, sq, sq);
+  c.fillRect(5 * sq, 12 * sq + 160, sq, sq);
+  c.fillRect(6 * sq, 12 * sq + 160, sq, sq);
+  c.lineWidth = 2;
+  c.strokeStyle = "black";
+  c.strokeRect(5 * sq, 11 * sq + 160, sq, sq);
+  c.strokeRect(6 * sq, 11 * sq + 160, sq, sq);
+  c.strokeRect(5 * sq, 12 * sq + 160, sq, sq);
+  c.strokeRect(6 * sq, 12 * sq + 160, sq, sq);
+
+  // c.clearRect(0, 0, sq * 10, sq * 20);
+};
+
 export const clearRowAnimation = rowNums => {
   const canvas = document.getElementById("tetris");
   const c = canvas.getContext("2d");
